b0921f318bc58e292ee533d6bfdd27f384d6e391
admin <admin@DESKTOP-I6HUGBR>
2025-12-04 14:15:08 +0900

    yourClass.java added

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,5 +4,5 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Bye!");
-		}
+	}
 }
---
+++ GitHW/src/mygit/yourClass.java
@@ -0,0 +1,7 @@
+package mygit;
+
+public class yourClass {
+	public void sayHello(String str) {
+		System.out.println("hello " + str);
+	}
+}
